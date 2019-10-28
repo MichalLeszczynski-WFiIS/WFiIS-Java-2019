6da02809d338fff67abda7ca636a16a1505c2391
Michal Leszczynski <misiek305@vp.pl>
2019-10-28 14:40:11 +0100

    after lab3

--- lab3/lab3string.java
+++ lab3/lab3string.java
@@ -27,6 +27,43 @@ public class lab3string
     {
         return  new StringBuilder(str).reverse().toString();
     }
-    
-    
+
+    public static boolean czy_plaindrom(String str)
+    {
+        return str.equals(new StringBuilder(str).reverse().toString());
+    }
+
+    public static boolean czy_abecadlowe(String str)
+    {
+        char before = str.charAt(0);
+        for(int i=1; i < str.length(); i ++)
+        {
+            if(str.charAt(i) < before ) return false;
+            before = str.charAt(i);
+        }
+        return true;
+    }
+    public static String rot13(String str)
+    {
+        StringBuilder new_string = new StringBuilder(); 
+        for(char a : str.toCharArray())
+        {
+            char new_char = a ;
+            int new_num = new_char;
+            if (new_char >= 'a'&& new_num <= 'z')
+            {
+                new_num += 13;
+                if(new_num > (int)'z') new_num -= 26;
+            }
+            else if (new_num >= 'A' && new_num <= 'Z')
+            {
+                new_num += 13;
+                if(new_num > (int)'Z') new_num -= 26;
+            }
+
+            new_char = (char)new_num;
+            new_string.append(new_char);
+        }
+        return new_string.toString();
+    }
 }
